33f35be1d737d0403bcbe98caa13f34b5f325ace
Joona Uutela <joona.uutela@gmail.com>
2020-03-30 19:19:33 +0300

    Completed excercise 'phonebook' (3.1-3.6)

---
+++ osa3/phonebook_server/index.js
@@ -0,0 +1,119 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    }
+]
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
+
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.get('/api/persons', (request, response) => {
+    response.send(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    console.log("Funtkiossa.")
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    const errorCheckResponse = handleErrorPost(body, response).statusCode
+
+    if (errorCheckResponse === 200) {
+        const personToCreate = {
+            name: body.name,
+            number: body.number,
+            id: generateId()
+        }
+
+        const duplicateCheckResponse = checkDuplicates(personToCreate, response).statusCode
+        if (duplicateCheckResponse === 200) {
+            persons = persons.concat(personToCreate)
+            response.json(personToCreate)
+        }
+    }
+})
+
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.send(
+        `<p>Phonebook has info for ${persons.length} people</<p><p>${date}</p>`
+    )
+})
+
+const handleErrorPost = (body, response) => {
+
+    // First condition checks for empty JSON
+    if (!Object.keys(body).length) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    } else if (!body.name || body.name === '') {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    } else if (!body.number || body.number === '') {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+    return response.status(200);
+}
+
+const checkDuplicates = (personToCreate, response) => {
+    if (persons.some(person => person.name === personToCreate.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    } else if (persons.some(person => person.number === personToCreate.number)) {
+        return response.status(400).json({
+            error: 'number must be unique'
+        })
+    } else {
+        return response.status(200)
+    }
+}
