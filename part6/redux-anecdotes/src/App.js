7bea81eb28ec8b8085a75ceadfc53f057208166c
Joona Uutela <joona.uutela@gmail.com>
2020-04-26 16:12:12 +0300

    Minor code cleanup on 'redux-anecdotes'

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -3,17 +3,12 @@ import { AnecdoteForm } from './components/AnecdoteForm'
 import { AnecdoteList } from './components/AnecdoteList'
 import { Notification } from './components/Notification'
 import Filter from './components/Filter'
-import store from './store'
 
 const App = () => {
-  const handleClick = () => {
-    console.log(store.getState())
-  }
   return (
     <div>
       <Notification />
       <h2>Anecdotes</h2>
-      <button onClick={() => handleClick()}>click me</button>
       <Filter />
       <AnecdoteList />
       <AnecdoteForm />
