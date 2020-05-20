c75cd1429a98d635335ef6f8fdc42e7acfd2805d
Joona Uutela <joona.uutela@gmail.com>
2020-05-20 10:48:58 +0300

    Minor modification on exercise 9.15

--- part9/my-app/src/components/Total.tsx
+++ part9/my-app/src/components/Total.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { CoursePart } from '../types'
+
 
 interface ContentProps {
-    courseParts: Array<{ name: string; exerciseCount: number }>;
+    courseParts: CoursePart[];
 }
 
 const Total: React.FC<ContentProps> = ({ courseParts }) => {
